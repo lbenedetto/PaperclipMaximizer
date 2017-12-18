e3b063e73acda78fc693e3d841578061e36055d9
larperdoodle <larsbenedetto@gmail.com>
2017-12-18 03:10:33 -0800

    Sort of works now. Needs tuning + parallelization

--- PaperclipMaximizer.js
+++ PaperclipMaximizer.js
@@ -1,100 +1,130 @@
 setTimeout(run, 1000);
+var numActions;
+var popSize = 20;
 
+var highscore = 0;
+//5 minutes per sim = 300 seconds
+//5000 ops per sim = 16 ops per second
+//20 population, 100 generations
+var clickRate = 62; //click once ever x ms
+var generation = 0;
+var startOps = 1000;
+var ops = startOps;
+var game, actions, numInputs, genetics;
+var k = 0;
+var networks = [];
 function run() {
-	var popSize = 20;
-	// Create an array to hold the population of networks
-	var networks = [];
-
-	// Populate the networks array
-	for (var i = 0; i < popSize; i++) {
-		networks.push(new Brainwave.Network(4, 1, 1, 4));
-	}
-	var highscore = 0;
-	// Next we need to create the Genetics object that will evolve the networks for us
-	var genetics = new Brainwave.Genetics(popSize, networks[0].getNumWeights());
-	var clickRate = 100; //click once ever x ms
-	var generation = 0;
-	var ops = 5000;
-	var game = $("#game")[0].contentWindow;
-	var actions = [
-		select("#btnMakePaperclip").click,
-		select("#btnLowerPrice").click,
-		select("#btnRaisePrice").click,
-		select("#btnExpandMarketing").click,
-		select("#btnBuyWire").click,
-		select("#btnMakeClipper").click,
-		select("#btnMakeMegaClipper").click,
-		select("#btnAddProc").click,
-		select("#btnAddMem").click,
-		select("#btnQcompute").click,
-		select("#btnInvest").click,
-		select("#btnWithdraw").click,
-		select("#btnImproveInvestments").click,
-		select("#btnNewTournament").click,
-		select("#btnRunTournament").click,
-		function () { select("#investStrat").val("low") },
-		function () { select("#investStrat").val("med") },
-		function () { select("#investStrat").val("hi") },
+	game = $("#game")[0].contentWindow;
+	actions = [
+		function () { select("#btnMakePaperclip").click() },
+		function () { select("#btnLowerPrice").click() },
+		function () { select("#btnRaisePrice").click() },
+		function () { select("#btnExpandMarketing").click() },
+		function () { select("#btnBuyWire").click() },
+		function () { select("#btnMakeClipper").click() },
+		function () { select("#btnMakeMegaClipper").click() },
+		function () { select("#btnAddProc").click() },
+		function () { select("#btnAddMem").click() },
+		function () { select("#btnQcompute").click() },
+		function () { select("#btnInvest").click() },
+		function () { select("#btnWithdraw").click() },
+		function () { select("#btnImproveInvestments").click() },
+		function () { select("#btnNewTournament").click() },
+		function () { select("#btnRunTournament").click() },
+		// function () { select("#investStrat").val("low") },
+		// function () { select("#investStrat").val("med") },
+		// function () { select("#investStrat").val("hi") },
 		//TODO: Add actions for picking strategic modeling strats
-		select("projectButton1").click,
-		select("projectButton2").click,
-		select("projectButton3").click,
-		select("projectButton4").click,
-		select("projectButton5").click,
-		select("projectButton6").click,
-		select("projectButton7").click,
-		select("projectButton8").click,
-		select("projectButton9").click,
-		select("projectButton10").click,
-		select("projectButton11").click,
-		select("projectButton12").click,
-		select("projectButton13").click,
-		select("projectButton14").click,
-		select("projectButton15").click,
-		select("projectButton16").click,
-		select("projectButton17").click,
-		select("projectButton18").click,
-		select("projectButton19").click,
-		select("projectButton20").click,
-		select("projectButton42").click,
-		select("projectButton21").click
+		function () { try { select("projectButton1").click() } catch (e) {} },
+		function () { try { select("projectButton2").click() } catch (e) {} },
+		function () { try { select("projectButton3").click() } catch (e) {} },
+		function () { try { select("projectButton4").click() } catch (e) {} },
+		function () { try { select("projectButton5").click() } catch (e) {} },
+		function () { try { select("projectButton6").click() } catch (e) {} },
+		function () { try { select("projectButton7").click() } catch (e) {} },
+		function () { try { select("projectButton8").click() } catch (e) {} },
+		function () { try { select("projectButton9").click() } catch (e) {} },
+		function () { try { select("projectButton10").click() } catch (e) {} },
+		function () { try { select("projectButton11").click() } catch (e) {} },
+		function () { try { select("projectButton12").click() } catch (e) {} },
+		function () { try { select("projectButton13").click() } catch (e) {} },
+		function () { try { select("projectButton14").click() } catch (e) {} },
+		function () { try { select("projectButton15").click() } catch (e) {} },
+		function () { try { select("projectButton16").click() } catch (e) {} },
+		function () { try { select("projectButton17").click() } catch (e) {} },
+		function () { try { select("projectButton18").click() } catch (e) {} },
+		function () { try { select("projectButton19").click() } catch (e) {} },
+		function () { try { select("projectButton20").click() } catch (e) {} },
+		function () { try { select("projectButton21").click() } catch (e) {} },
+		function () { try { select("projectButton42").click() } catch (e) {} }
 		//TODO: add the rest of the project buttons
 	];
+	numActions = actions.length;
 	game.reset();
-	var k = 0;
+	k = 0;
+	// Create an array to hold the population of networks
+	networks = [];
+	// Populate the networks array
+	numInputs = getGameState().length;
+	//Network(numInputs, numOutputs, numHiddenLayers, numNeuronsPerHiddenLayer);
+	for (var i = 0; i < popSize; i++) {
+		networks.push(new Brainwave.Network(numInputs, 1, 3, numInputs));
+	}
+	// Next we need to create the Genetics object that will evolve the networks for us
+	genetics = new Brainwave.Genetics(popSize, networks[0].getNumWeights());
 	networks[k].importWeights(genetics.population[k].weights);
-	var timer = setInterval(function () {
-		if (generation > 1000) {//Done with all generations
-			//Done evolving
-			clearInterval(timer);
-			//TODO: Save best network
-		} else if (k > popSize) {//Done with all networks in current generation
-			//Evolve the networks based on the fitness results
-			genetics.epoch(genetics.population);
-			//Done with this generation
-			generation++;
-			k = 0;
-		} else if (ops < 0) {//Done with current network
-			var score = toInt($("#clips").innerHTML);
-			if (score > highscore) {
-				console.log("Generation " + generation + "." + k + " achieved new highscore of " + score);
-				highscore = score;
+	setTimeout(work, clickRate);
+}
+var isFirstAction = true;
+function work() {
+	if (generation >= 1000) {//Done with all generations
+		//Save best network
+		var bestFit = 0;
+		var bestFitIx = 0;
+		for(var n = 0; n < popSize; n++){
+			if(genetics.population[n].fitness > bestFit){
+				bestFitIx = n;
+				bestFit = genetics.population[n].fitness;
 			}
-			genetics.population[k].fitness = score;
-			k++;
-			networks[k].importWeights(genetics.population[k].weights);
-			ops = 5000;
-			game.reset();
-		} else {//Run current network
-			ops--;
-			//Pass game board array into the run function
-			var chosenAction = networks[k].run(getGameState());
-			console.log(chosenAction);
-			actions[chosenAction]();
 		}
-
-	}, clickRate);
-};
+		console.log(JSON.stringify(networks[bestFitIx]))
+	} else if (k >= popSize) {//Done with all networks in current generation
+		//Evolve the networks based on the fitness results
+		genetics.epoch(genetics.population);
+		//Done with this generation
+		generation++;
+		k = 0;
+		setTimeout(work, clickRate)
+	} else if (ops <= 0) {//Done with current network
+		var score = toInt(select("#clips")) + toInt(select("#funds"));
+		if (score >= highscore) {
+			console.log("Generation " + generation + "." + k + " achieved new highscore of " + score);
+			highscore = score;
+		}
+		genetics.population[k].fitness = score;
+		k++;
+		if(k < popSize)
+			networks[k].importWeights(genetics.population[k].weights);
+		ops = startOps;
+		isFirstAction = true;
+		game.reset();
+		setTimeout(work, clickRate + 1000)
+	} else {//Run current network
+		ops--;
+		//Pass game board array into the run function
+		var chosenAction = scaleOutput(networks[k].run(getGameState()));
+		console.log(chosenAction);
+		if(isFirstAction){
+			isFirstAction = false;
+			if(chosenAction !== 0) ops = 0;
+		}
+		actions[chosenAction]();
+		setTimeout(work, clickRate)
+	}
+}
+function scaleOutput(output){
+	return Math.floor(output[0] * numActions);
+}
 
 function getGameState() {
 	return [
@@ -116,30 +146,48 @@ function getGameState() {
 		toInt(select("#memory")),
 		toInt(select("#operations")),
 		toInt(select("#creativity")),
-		select("#qChip0").css("opacity"),
-		select("#qChip1").css("opacity"),
-		select("#qChip2").css("opacity"),
-		select("#qChip3").css("opacity"),
-		select("#qChip4").css("opacity"),
-		select("#qChip5").css("opacity"),
-		select("#qChip6").css("opacity"),
-		select("#qChip7").css("opacity"),
-		select("#qChip8").css("opacity"),
-		select("#qChip9").css("opacity"),
-		parseInt(select("#qCompDisplay").innerHTML.replace("qOps: ", "")),
-		toInt(select("#investmentBankroll").innerHTML),
-		toInt(select("#secValue").innerHTML),
-		toInt(select("#portValue").innerHTML),
-		toInt(select("#investmentLevel").innerHTML),
-		toInt(select("#investUpgradeCost").innerHTML),
-		toInt(select("#yomiDisplay").innerHTML),
-		toInt(select("#newTourneyCost").innerHTML),
+		getQChipValue("#qChip0"),
+		getQChipValue("#qChip1"),
+		getQChipValue("#qChip2"),
+		getQChipValue("#qChip3"),
+		getQChipValue("#qChip4"),
+		getQChipValue("#qChip5"),
+		getQChipValue("#qChip6"),
+		getQChipValue("#qChip7"),
+		getQChipValue("#qChip8"),
+		getQChipValue("#qChip9"),
+		// getQCompDisplay(),
+		// toInt(select("#investmentBankroll").innerHTML),
+		// toInt(select("#secValue").innerHTML),
+		// toInt(select("#portValue").innerHTML),
+		// toInt(select("#investmentLevel").innerHTML),
+		// toInt(select("#investUpgradeCost").innerHTML),
+		// toInt(select("#yomiDisplay").innerHTML),
+		// toInt(select("#newTourneyCost").innerHTML),
 	];
 }
 
+function getQCompDisplay () {
+	var val = parseInt(select("#qCompDisplay").innerHTML.replace("qOps: ", ""))
+	if(isNaN(val)) return 0;
+}
+function getQChipValue(target){
+	try {
+		return select("#qChip0").css("opacity");
+	} catch (e) {
+		return 2;
+	}
+}
+
 function toInt(input) {
-	var ht = input.innerHTML;
-	return parseFloat(ht.replace(/,/g, ''));
+	try {
+		var ht = input.innerHTML;
+		var val =  parseFloat(ht.replace(/,/g, ''));
+		if(isNaN(val)) return 0;
+		return val;
+	} catch (e) {
+		return 0;
+	}
 }
 
 var context = window.parent.frames[0].document;
